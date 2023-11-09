05dd6b1802dd73c3e2032767dc22f50ae2e011d8
Daniel Y <{ID}+{username}@users.noreply.github.com>
2023-11-09 11:06:59 -0600

    Remove unused deps and update readme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import '../styles/globals.css';
 import { ThemeProvider } from "@material-tailwind/react";
 import { SessionProvider } from "next-auth/react";
 import { DM_Sans } from 'next/font/google';
-import { RecoilRoot } from 'recoil';
 const dm = DM_Sans({
   weight: ['300', '400', '500'],
   style: ['normal', 'italic'],
@@ -14,14 +13,11 @@ const dm = DM_Sans({
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <RecoilRoot>
         <main className={dm.className}>
-
           <ThemeProvider>
             <Component {...pageProps} />
           </ThemeProvider>
         </main>
-      </RecoilRoot>
     </SessionProvider>
   )
 }
